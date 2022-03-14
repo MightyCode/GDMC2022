eed727faa956ecd9d88709b8ebd39491cc3f9786
Maxence Bazin <bazinmaxence@free.fr>
2022-03-14 21:44:35 +0100

    add class for settlement representation and not only generation

---
+++ representation/Village.py
@@ -0,0 +1,5 @@
+class Village:
+    def __init__(self) -> None:
+        self.name:str = ""
+        self.biomeId = 0
+        self.tier = 0
---
+++ representation/Villager.py
@@ -0,0 +1,3 @@
+class Villager:
+    def __init__(self) -> None:
+        self.name:str = ""
